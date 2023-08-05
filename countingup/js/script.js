d107849da26d6e96d09c2716e817f717b4066109
St Akhter(Tanha) <128251318+stakhter@users.noreply.github.com>
2023-08-06 03:40:29 +0600

    Add files via upload

---
+++ countingup/js/script.js
@@ -0,0 +1,47 @@
+//js code
+
+
+
+
+
+
+//jq code
+$(document).ready(function(){
+	// countup
+	$(".count").each(function () {
+
+ 	 $(this)
+
+	    .prop("Counter", 0)
+
+	    .animate(
+
+	    {
+
+	        Counter: $(this).text(),
+
+	      },
+
+	      {
+
+	        duration: 4000,
+
+	        easing:"swing",
+
+	        step:function (now) {
+
+	         now = Number(Math.ceil(now)).toLocaleString('en');
+
+             $(this).text(now);
+
+        },
+
+      },
+
+    );
+
+});
+
+
+	
+})
---
+++ js/script.js
@@ -0,0 +1,21 @@
+//js code
+
+
+
+
+
+
+//jq code
+$(document).ready(function(){
+	// counter
+	$('.counter').counterUp({
+	    delay: 10,
+	    time: 1000
+	});
+	// animated
+	$(".num").numScroll({
+      number:123456,
+      symbol: true
+    });
+	
+});
